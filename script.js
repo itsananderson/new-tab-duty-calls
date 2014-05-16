69dbb73e1d1ac45c2d68cd033d15adb9afee7e7b
Will Anderson <wiand@microsoft.com>
2014-05-16 09:31:04 -0700

    Center image in new tab page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function getUrl() {
     chrome.tabs.getSelected(null, function (tab){
-        document.body.innerHTML = "<img src='http://imgs.xkcd.com/comics/duty_calls.png' />";
+        document.body.innerHTML = '<img src="http://imgs.xkcd.com/comics/duty_calls.png" style="position: absolute; display: block; width: 300px; height: 330px; top: 50%; left: 50%; margin: -165px -150;" />';
     })
 })();
